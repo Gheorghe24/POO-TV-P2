2d82a1722dbb9563193cc13f1cd9180b6824de89
Gheorghe24 <gheorghe231101@yahoo.com>
2023-01-10 23:18:22 +0200

    Added Builder Default annotations for Movie(60p)

--- src/io/Movie.java
+++ src/io/Movie.java
@@ -17,11 +17,17 @@ public final class Movie {
     private String name;
     private String year;
     private Integer duration;
+    @Builder.Default
     private ArrayList<String> genres = new ArrayList<>();
+    @Builder.Default
     private ArrayList<String> actors = new ArrayList<>();
+    @Builder.Default
     private ArrayList<String> countriesBanned = new ArrayList<>();
+    @Builder.Default
     private Integer numLikes = 0;
+    @Builder.Default
     private Double rating = 0.00;
+    @Builder.Default
     private Integer numRatings = 0;
 
     public Movie(final Movie movie) {
